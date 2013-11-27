145d69d926eb08b5ee613372fc5bd64d1acdbc67
Nicholas Land <ndland@oakland.edu>
2013-11-27 15:18:08 -0500

    added git submodules script

---
+++ get_submodules.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git submodule add https://github.com/Yggdroot/indentLine.git
+git submodule add https://github.com/scrooloose/nerdtree.git
+git submodule add https://github.com/tomtom/tcomment_vim.git
+git submodule add https://github.com/tpope/vim-fugitive.git
+git submodule add https://github.com/airblade/vim-gitgutter.git
+git submodule add https://github.com/jelera/vim-javascript-syntax.git
+git submodule add https://github.com/Lokaltog/vim-powerline.git
