686d5d9cb25edc9271215db9d1d6a1dced72408c
Nicholas Land <ndland@oakland.edu>
2014-03-21 16:01:05 -0400

    No more submodules

---
+++ update_vim_plugins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+cd ~/dotfiles/vim/bundle/
+
+rm -rf --interactive=never *
+
+git clone https://github.com/Yggdroot/indentLine.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/tomtom/tcomment_vim.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/jelera/vim-javascript-syntax.git
+git clone https://github.com/msanders/snipmate.vim.git
+git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/bronson/vim-trailing-whitespace.git
