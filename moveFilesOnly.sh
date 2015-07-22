200d1495f632378a83624faea3d7ce1049bea240
Nicholas Land <nland@authen2cate.com>
2015-07-22 11:39:25 -0400

    Updated script for directories, and redirect output to null.

--- moveFilesOnly.sh
+++ moveFilesOnly.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles
+# in ~/dotfiles
 # AUTHOR Michael Smalley
+# UPDATED BY Nicholas Land
 ############################
 
 ########## Variables
@@ -8,13 +10,15 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 confg=~/.config
-if [ `uname` == 'Darwin' ]; then
-  files="tmux.conf gitconfig zlogin vimrc zshrc vim slate"
-else
-  files="vrapperrc tmux.conf ackrc xinitrc termconfig gitconfig zshenv rc.lua zlogin vimrc zshrc vim"
-fi
 
-##########
+# If you're on a mac, otherwise, assume Linux.
+if [ `uname` == 'Darwin' ]; then
+  files="vrapperrc tmux.conf gitconfig zlogin vimrc zshrc"
+  directories="vim atom"
+else
+  files="vrapperrc tmux.conf ackrc xinitrc termconfig gitconfig zshenv
+         rc.lua zlogin vimrc zshrc vim"
+fi
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -26,20 +30,31 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+# move any existing dotfiles in homedir to dotfiles_old directory,
+# then create symlinks
     echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
     echo "Creating symlink to $file in home directory."
     if [ $file == rc.lua ]; then
-      mv ~/$file ~/dotfiles_old/
-      mkdir -p ~/.config/awesome
-      ln -s $dir/$file ~/.config/awesome
+      mv ~/$file ~/dotfiles_old/ > /dev/null 2>&1
+      mkdir -p ~/.config/awesome > /dev/null 2>&1
+      ln -s $dir/$file ~/.config/awesome > /dev/null 2>&1
     elif [ $file == termconfig ]; then
-      mkdir -p ~/.config/terminator/
-      ln -s $dir/$file ~/.config/terminator/config
+      mkdir -p ~/.config/terminator/ > /dev/null 2>&1
+      ln -s $dir/$file ~/.config/terminator/config > /dev/null 2>&1
     else
-      mv ~/.$file ~/dotfiles_old/
-      ln -s $dir/$file ~/.$file
+      mv ~/.$file ~/dotfiles_old/ > /dev/null 2>&1
+      ln -s $dir/$file ~/.$file > /dev/null 2>&1
     fi
 done
 
+# Move any existing directories out of homedir to dotfiles_old, and create some
+# new symlinks.
+for directory in $directories; do
+  echo "Moving $directory to ~/dotfiles_old"
+  mv ~/.$directory ~/dotfiles_old > /dev/null 2>&1
+  echo "Symlinking $directory to your home directory"
+  ln -s $dir/$directory ~/.$directory > /dev/null 2>&1
+done
+
+echo "All done!"
